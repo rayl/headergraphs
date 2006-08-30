16c376cb9e253faba648c70277a6577e42d9c4a8
Ray Lehtiniemi <rayl@mail.com>
2006-08-29 21:18:58 -0600

    Move the inclusion information gathering into a module.
    Signed-off-by: Ray Lehtiniemi <rayl@mail.com>

--- graph.pl
+++ graph.pl
@@ -5,187 +5,20 @@ use strict;
 use warnings;
 use lib qw(.);
 
-use Benchmark;
 use Data::Dumper;
-use Cwd;
 
 use Graph;
 use Analysis;
 use Dot;
+use Gather;
 
-# the location of the linux kernel tree
-my $tree = "/home/rayl/proj/linux-headers";
-
-
-# the specific asm-* directories of interest.
-# the heuristic used by we_want_hdr_dir() is imperfect, but
-# should be adequate for i386, arm, mips, and cris, at least...
-my ($arch, $mach) = 
-	#("arm",   "ep93xx");
-	#("arm",   "versatile");
-	#("i386",  "generic");
-	("x86_64",  "");
-
-
-# the forward (includes) digraph
-my $g = Graph->new;
-
-
-
-#
-# Decide whether we want to process a header directory.
-#
-sub we_want_hdr_dir
-{
-	my ($x) = @_;
-	return 0 if       $x =~ m,^config(/.*)?$,;
-	return 1 if       $x =~ m,^asm-generic$,;
-	return 0 if       $x =~ m,^asm-,;
-	return 0 if       $x =~ m,^asm/(mach|arch)-,;
-	return 1;
-}
-
-#
-# Find all interesting header directories in the include tree
-#
-sub interesting_hdr_dirs
-{
-	my @d = `find . -type d -o -type l`;
-	map {s/^..//} @d;
-	chomp @d;
-	sort grep {we_want_hdr_dir $_} @d;
-}
-
-#
-# Routine to decide whether we want to process a header file
-#
-sub we_want_hdr_file
-{
-	my ($x) = @_;
-	return 0 if $x =~ m,^asm.*/asm-offsets.h$,;
-	return 0 if $x =~ m,^linux/autoconf.h$,;
-	return 0 if $x =~ m,^linux/compile.h$,;
-	return 0 if $x =~ m,^linux/compiler.h$,;
-	return 0 if $x =~ m,^linux/version.h$,;
-	return 1;
-}
-
-#
-# Find all interesting header files in a directory
-#
-sub interesting_hdr_files
-{
-	my ($d) = @_;
-	sort grep {we_want_hdr_file $_} <$d/*.h>;
-}
-
-#
-# Decide whether we want to process a source directory.
-#
-sub we_want_src_dir
-{
-	my ($x) = @_;
-	return 0 if       $x =~ m,^Documentation/,;
-	return 0 if       $x =~ m,^\.git/,;
-	return 0 if       $x =~ m,^include/,;
-	return 0 if       $x =~ m,^scripts/,;
-	return 0 if       $x =~ m,^\.tmp_versions/,;
-	return 0 if       $x =~ m,^usr/,;
-	return 1 unless   $x =~ m,^arch/,;
-	return 0 unless   $x =~ m,^arch/$arch(/.*)?$,;
-	return 1;
-}
-
-#
-# Find all interesting source directories in the include tree
-#
-sub interesting_src_dirs
-{
-	my @d = `find . -type d`;
-	map {s/^..//} @d;
-	chomp @d;
-	sort grep {we_want_src_dir $_} @d;
-}
-
-#
-# Routine to decide whether we want to process a source file
-#
-sub we_want_src_file
-{
-	my ($x) = @_;
-	return 1;
-}
-
-#
-# Find all interesting source files in a directory
-#
-sub interesting_src_files
-{
-	my ($d) = @_;
-	sort grep {we_want_src_file $_} <$d/*.c>;
-}
-
-#
-# Extract include lines from a file
-#
-sub gather
-{
-	my ($file) = @_;
-
-	# bail if we've already parsed this file for some reason
-	my $n = $g->node($file);
-	return if $n->{'parsed'};
-
-	# remember that we've parsed this file
-	$n->{'parsed'} = 1;
-
-	# read the raw #include lines
-	open F, "<$file" || die "Can't read $file!";
-	my @incs = grep {s,^\s*#\s*include\s*(["<][^>"]*[">]).*,$1,} <F>;
-	chomp @incs;
-	close F;
-
-	# add current directory to #include "" lines
-	my $dir = $file;
-	$dir =~ s,(.*)/.*,$1,;
-	map {s,","$dir/,} @incs;
-
-	# trim the <> or "" characters
-	map {s/^.//} @incs;
-	map {s/.$//} @incs;
-
-	# create edges for each inclusion
-	for my $h (@incs)
-	  {
-		$g->edge($file, $h);
-	  }
-}
+# the raw inclusion information
+my $g;
 
 
 sub do_it
 {
-	my ($t0, $t1);
-	my $c = cwd();
-
-	$g = Graph->new;
-
-	$t0 = new Benchmark;
-
-	print "process hdrs\n";
-	chdir "$tree/include" || die "Bad tree: $tree";
-	map {gather $_} map {interesting_hdr_files $_} interesting_hdr_dirs;
-	$t1 = new Benchmark;
-	print "Took " . timestr(timediff($t1, $t0)) . " seconds\n";
-	$t0 = $t1;
-
-	print "process src\n";
-	chdir "$tree" || die "Bad tree: $tree";
-	map {gather $_} map {interesting_src_files $_} interesting_src_dirs;
-	$t1 = new Benchmark;
-	print "Took " . timestr(timediff($t1, $t0)) . " seconds\n";
-	$t0 = $t1;
-
-	chdir $c;
+	$g = Gather::do_it();
 }
 
 sub save_it
