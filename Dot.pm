713e69438106764f6425ea5585886dd50981bf46
Ray Lehtiniemi <rayl@mail.com>
2006-09-18 20:59:44 -0600

    Extensive refactoring and commenting of Dot graph generation.
    Signed-off-by: Ray Lehtiniemi <rayl@mail.com>

--- Dot.pm
+++ Dot.pm
@@ -1,3 +1,6 @@
+#
+#  Dot.pm - Display an Analysis object using the 'dot' language
+#
 
 use 5.6.0;
 use strict;
@@ -7,11 +10,11 @@ package Dot;
 
 sub node_color
 {
-	my ($w) = @_;
-	if     ($w <  25) { undef }
-	elsif  ($w <  50) { "d0" }
-	elsif  ($w < 100) { "80" }
-	elsif  ($w < 150) { "40" }
+	my ($weight) = @_;
+	if     ($weight <  25) { undef }
+	elsif  ($weight <  50) { "d0" }
+	elsif  ($weight < 100) { "80" }
+	elsif  ($weight < 150) { "40" }
 	else              { "00" }
 }
 
@@ -27,32 +30,42 @@ sub blue_color
 	"#${c}${c}ff";
 }
 
-sub snipped
+sub should_snip
 {
-	my ($f, $cuts, $c) = @_;
-	exists $cuts->{$f} && ((not defined $c) || ($cuts->{$f} > 4));
+	my ($a, $target) = @_;
+
+	# figure out how large the subtree rooted at the target is
+	my $weight = $a->{'graph'}->unique_tsize($target);
+
+	# check whether we want to color the target node
+	my $color = node_color($weight);
+
+	# bind a local name to the list of potential cut points
+	my $cuts = $a->{'cuts'};
+
+	# we should snip the edge if the target node qualifies for cutting
+	# (in other words, if it has too many incoming edges), FIXME
+	exists $cuts->{$target} && ((not defined $color) || ($cuts->{$target} > 4));
 }
 
-sub graph_node
+sub print_node
 {
-	my ($g, $map, $root, $node, $cuts, $total) = @_;
+	my ($a, $node) = @_;
 
-	return if $map->{$node};
-	$map->{$node} = 1;
-
-	my $t = $total->{$node} || "?";
+	my $g = $a->{'graph'};
+	my $t = $g->total_tsize($a->{'file'})->{$node} || "?";
 	my $n = $g->unique_tsize($node);
 	my $c = node_color($n);
 	my $o = octo_color($c) if defined $c;
 	my $b = blue_color($c) if defined $c;
 
-	if ($root eq $node)
+	if ($node eq $a->{'file'})
 	  {
 		print "\t\"$node\" [label=\"$node\\n($n/$t)\", shape=house, color=\"#0000ff\", fillcolor=\"#ffff00\", style=filled];\n";
 	  }
-	elsif (snipped($node, $cuts, $c))
+	elsif (should_snip($a, $node))
 	  {
-		my $m = $cuts->{$node};
+		my $m = $a->{'cuts'}->{$node};
 		if ($g->children($node))
 		  {
 			print "\t\"$node\" [label=\"<$m times>\\n$node\\n($n/$t)\"";
@@ -87,41 +100,106 @@ sub graph_node
 	  }
 }
 
-sub unique_tsize_len
+#
+# Print all nodes.
+#
+sub print_nodes
 {
-	my ($w) = @_;
-	if     ($w <  10) { 5.0 }
-	elsif  ($w <  30) { 3.0 }
-	elsif  ($w < 100) { 1.0 }
-	elsif  ($w < 150) { 0.5 }
-	else              { 0.1 }
+	my ($a) = @_;
+
+	# walk over each node in the mesh
+	for my $node (keys %{$a->{'nodelist'}})
+	  {
+		print_node($a, $node);
+	  }
 }
 
-sub graph_edge
+#
+# Decide how long an edge should be, based on how large is the subtree
+# rooted at the target of the edge.  Edges pointing to relatively small
+# subtrees can be long, while edges pointing to large subtrees will be
+# short.  this is useful for the radial graph layout.  it helps the "heavy"
+# nodes with large subtrees to cluster near the center of the spider web.
+#
+sub edge_length
 {
-	my ($g, $e, $f, $cuts, $m2) = @_;
-	my $w = $g->unique_tsize($f);
-	my $c = node_color($w);
-	my $l = unique_tsize_len($w);
+	my ($weight) = @_;
+	if     ($weight <  10) { 5.0 }
+	elsif  ($weight <  30) { 3.0 }
+	elsif  ($weight < 100) { 1.0 }
+	elsif  ($weight < 150) { 0.5 }
+	else                   { 0.1 }
+}
 
-	if (snipped($f, $cuts, $c))
+#
+# Print a single edge.
+#
+sub print_edge
+{
+	my ($a, $source, $target, $ghost) = @_;
+
+	# pick a length for this edge, based on the size of the subtree
+	# rooted at the target node.
+	my $weight = $a->{'graph'}->unique_tsize($target);
+	my $length = edge_length($weight);
+
+	# check whether edges to this target should be snipped or not.
+	if (should_snip($a, $target))
 	  {
-		$m2->{$f}++;
-		if ($m2->{$f} == 1)
-		  {
-			print "\t\"$e\" -> \"$f\" [len=$l];\n";
-		  }
-		else
-		  {
-			print "\t\"$e\" -> \"$f/" . $m2->{$f} . "\" [len=$l];\n";
-		  }
+		# increment the number of ghost nodes we have generated for
+		# this snipped node
+		$ghost->{$target}++;
+	  }
+
+	# decide whether to actually snip this particular edge or not.
+	if (($ghost->{$target} || 1) < 2)
+	  {
+		# if target is not a candidate for snipping, or if this is the
+		# first time we're printing a snippable target node, then do not
+		# snip the edge.  for snippable targets, this will bind the node
+		# into the main tree instead of letting it possibly float off to
+		# the right edge of the page.
+		print "\t\"$source\" -> \"$target\" [len=$length];\n";
 	  }
 	else
 	  {
-		print "\t\"$e\" -> \"$f\" [len=$l];\n";
+		# if we've already printed the target node once, snip the edge by
+		# generating a unique ghost node for the snipped edge to point at
+		print "\t\"$source\" -> \"$target/" . $ghost->{$target} . "\" [len=$length];\n";
 	  }
 }
 
+#
+# Print all edges.
+#
+sub print_edges
+{
+	my ($a) = @_;
+
+	# a hash used to uniquely number ghost nodes
+	my %ghost;
+
+	# bind a local name to the analysis mesh object
+	my $mesh = $a->{'mesh'};
+
+	# walk over each source node
+	for my $source (sort keys %$mesh)
+	  {
+		# walk over each target node for this source
+		for my $target (keys %{$mesh->{$source}})
+		  {
+			# process the edge from source to target
+			print_edge($a, $source, $target, \%ghost);
+		  }
+	  }
+}
+
+#
+# Print the graph header.
+#
+# Open a directed graph object and set up the graph
+# options.
+#
 sub print_ghead
 {
 	my ($a) = @_;
@@ -132,48 +210,32 @@ sub print_ghead
 	print "\troot=\"$file\";\n";
 }
 
-sub print_edges
-{
-	my ($a) = @_;
-	my ($g, $file, $mesh, $cuts) = ($a->{'graph'}, $a->{'file'}, $a->{'mesh'}, $a->{'cuts'});
-	my %m2;
-	for my $e (sort keys %$mesh)
-	  {
-		for my $f (keys %{$mesh->{$e}})
-		  {
-			graph_edge($g, $e, $f, $cuts, \%m2);
-		  }
-	  }
-}
-
-sub print_nodes
-{
-	my ($a) = @_;
-	my ($g, $file, $mesh, $cuts) = ($a->{'graph'}, $a->{'file'}, $a->{'mesh'}, $a->{'cuts'});
-	my %n;
-	my $t = $g->total_tsize($file);
-	for my $e (sort keys %$mesh)
-	  {
-		graph_node($g, \%n, $file, $e, $cuts, $t);
-		for my $f (sort {$g->unique_tsize($b) <=> $g->unique_tsize($a)} keys %{$mesh->{$e}})
-		  {
-			graph_node($g, \%n, $file, $f, $cuts, $t);
-		  }
-	  }
-}
-
+#
+# Print the graph footer.
+#
+# Close off the digraph object.
+#
 sub print_gfoot
 {
 	my ($a) = @_;
 	print "}\n";
 }
 
+#
+# Generate a dot graph in four sections:  A header, followed
+# by all edges, then all nodes, and finally a footer.
+#
+# The input is an analysis object, which has extracted some
+# useful bits of information about the topology of an
+# underlying graph object.
+#
 sub graph2
 {
-	print_ghead(@_);
-	print_edges(@_);
-	print_nodes(@_);
-	print_gfoot(@_);
+	my ($a) = @_;
+	print_ghead($a);
+	print_edges($a);
+	print_nodes($a);
+	print_gfoot($a);
 }
 
 1;
